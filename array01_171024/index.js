1a594e1d11355b8b5842fb6e08ded724327b9c31
VitorSilva87 <vitormanuel_araujo@hotmail.com>
2024-10-18 00:05:10 +0100

    Add files via upload

---
+++ array01_171024/index.js
@@ -0,0 +1,15 @@
+ // Criando um array com idades
+ let idades = [19,21,33,52,37,44,49,31,40,21,33,38]; // Array de 10 idades
+
+ let soma = 0;
+
+ 
+ for (let i = 0; i < idades.length; i++) {
+   soma += idades  [i];
+     
+ }
+
+ let media = soma / idades.length;
+
+console.log(media);
+ 
